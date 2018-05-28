edae69fc1a7b08cb692a40567e923c36df67cee7
Vu Nguyen <nbtvu@users.noreply.github.com>
2018-05-28 18:45:17 +0700

    Update radixsort.py

--- radixsort.py
+++ radixsort.py
@@ -1,17 +1,18 @@
+def countingSort(arr, l, div):
+    counts = [0] * 10
+    for i in range(l):
+        counts[arr[i] // div % 10] += 1
+    for i in range(1, 10):
+        counts[i] += counts[i - 1]
+    output = [0] * l
+    for i in reversed(range(l)):
+        d = arr[i] // div % 10
+        output[counts[d] - 1] = arr[i]
+        counts[d] -= 1
+    for i in range(l):
+        arr[i] = output[i]
+        
 def RadixSort(arr):
-    def countingSort(arr, l, div):
-        counts = [0] * 10
-        for i in range(l):
-            counts[arr[i] // div % 10] += 1
-        for i in range(1, 10):
-            counts[i] += counts[i - 1]
-        output = [0] * l
-        for i in reversed(range(l)):
-            d = arr[i] // div % 10
-            output[counts[d] - 1] = arr[i]
-            counts[d] -= 1
-        for i in range(l):
-            arr[i] = output[i]
     l = len(arr)
     maxEle = max(arr)
     div = 1
